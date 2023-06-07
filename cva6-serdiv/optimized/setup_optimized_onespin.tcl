cce8074adac6a0085fe12e6543d3ba92ee9c70ef
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2023-06-07 09:58:17 +0200

    First optimized version with 1 instead of WIDTH+2 cycle delay when b=0. Also added (still unused) signal for detecting b=-1

---
+++ cva6-serdiv/optimized/setup_optimized_onespin.tcl
@@ -0,0 +1,24 @@
+# @lang=tcl @ts=8
+
+set script_path [file dirname [file normalize [info script]]]
+
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/../common/cf_math_pkg.sv}
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/../common/rvfi_pkg.sv}
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/../common/riscv_pkg.sv}
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/../common/ariane_dm_pkg.sv}
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/../common/ariane_pkg.sv}
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/../common/cv64a6_imafdc_sv39_config_pkg.sv}
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/../common/lzc.sv}
+read_verilog -golden  -pragma_ignore {}  -version sv2012 {$script_path/serdiv.sv}
+
+set_elaborate_option -golden -verilog_parameter {WIDTH=8}
+
+elaborate -golden
+
+compile -golden
+
+set_mode mv
+
+read_sva -version {sv2012} {$script_path/serdiv.sva}
+
+check -all [get_checks]
