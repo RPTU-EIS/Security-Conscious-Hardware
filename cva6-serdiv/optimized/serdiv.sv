8042983d3c25220575f1a3fc1411bb7beff3a9f5
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2023-06-07 11:18:51 +0200

    Implemented fast path for b=-1

--- cva6-serdiv/optimized/serdiv.sv
+++ cva6-serdiv/optimized/serdiv.sv
@@ -47,7 +47,7 @@ module serdiv import ariane_pkg::*; #(
   logic [WIDTH-1:0]       op_b_q, op_b_d;
   logic                   op_a_sign, op_b_sign;
   logic                   op_b_zero, op_b_zero_q, op_b_zero_d;
-  logic                   op_b_neg1, op_b_neg1_q, op_b_neg1_d;
+  logic                   op_b_neg_one, op_b_neg_one_q, op_b_neg_one_d;
 
   logic [TRANS_ID_BITS-1:0] id_q, id_d;
 
@@ -61,13 +61,13 @@ module serdiv import ariane_pkg::*; #(
   logic [WIDTH-1:0] b_mux;
   logic [WIDTH-1:0] out_mux;
 
-  logic [$clog2(WIDTH+1)-1:0] cnt_q, cnt_d;
+  logic [$clog2(WIDTH)-1:0] cnt_q, cnt_d;
   logic cnt_zero;
 
   logic [WIDTH-1:0] lzc_a_input, lzc_b_input, op_b;
   logic [$clog2(WIDTH)-1:0] lzc_a_result, lzc_b_result;
   logic [$clog2(WIDTH+1)-1:0] shift_a;
-  logic [$clog2(WIDTH+1):0] div_shift;
+  logic [$clog2(WIDTH+1)-1:0] div_shift;
 
   logic a_reg_en, b_reg_en, res_reg_en, ab_comp, pm_sel, load_en;
   logic lzc_a_no_one, lzc_b_no_one;
@@ -79,14 +79,13 @@ module serdiv import ariane_pkg::*; #(
 // for faster division
 /////////////////////////////////////
 
-  assign op_b_zero = (op_b_i == '0);
-  assign op_b_neg1 = (op_b_i == '1) && opcode_i[0];
+  assign op_b_zero    = (op_b_i == '0);
+  assign op_b_neg_one = (op_b_i == '1) && opcode_i[0];
   assign op_a_sign = op_a_i[$high(op_a_i)];
   assign op_b_sign = op_b_i[$high(op_b_i)];
 
   assign lzc_a_input = (opcode_i[0] & op_a_sign) ? {~op_a_i[$high(op_a_i)-1:0], 1'b1} : op_a_i; 
-  // This change in lzc_a_input equation helps to fix erronous results for DIV and REM for -1/1.
-  assign lzc_b_input = (opcode_i[0] & op_b_sign) ? ~op_b_i         : op_b_i;
+  assign lzc_b_input = (opcode_i[0] & op_b_sign) ? ~op_b_i                            : op_b_i;
 
   lzc #(
     .MODE    ( 1          ), // count leading zeros
@@ -106,13 +105,13 @@ module serdiv import ariane_pkg::*; #(
     .empty_o ( lzc_b_no_one )
   );
 
-  assign shift_a      = (lzc_a_no_one) ? WIDTH         : lzc_a_result;
-  assign div_shift    = (lzc_b_no_one) ? WIDTH-shift_a : lzc_b_result-shift_a;
+  assign shift_a      = (lzc_a_no_one) ? WIDTH : lzc_a_result;
+  assign div_shift    = lzc_b_result - shift_a;
 
   assign op_b         = op_b_i <<< $unsigned(div_shift);
 
   // the division is zero if |opB| > |opA| and can be terminated
-  assign div_res_zero_d = (load_en) ? ($signed(div_shift) < 0) : div_res_zero_q;
+  assign div_res_zero_d = (load_en) ? div_shift[$high(div_shift)] : div_res_zero_q;
 
 /////////////////////////////////////
 // Datapath
@@ -127,7 +126,7 @@ module serdiv import ariane_pkg::*; #(
   assign b_mux       = (load_en)   ? op_b : {comp_inv_q, (op_b_q[$high(op_b_q):1])};
 
   // in case of bad timing, we could output from regs -> needs a cycle more in the FSM
-  assign out_mux     = (rem_sel_q) ? op_a_q : (op_b_zero_q ? '1 : res_q);
+  assign out_mux     = (rem_sel_q) ? (op_b_neg_one_q ? '0 : op_a_q) : (op_b_zero_q ? '1 : (op_b_neg_one_q ? -$signed(op_a_q) : res_q));
 
   // invert if necessary
   assign res_o       = (res_inv_q) ? -$signed(out_mux) : out_mux;
@@ -170,13 +169,13 @@ module serdiv import ariane_pkg::*; #(
         end
       end
       DIVIDE: begin
-        if(~div_res_zero_q & ~op_b_zero_q) begin
+        if (~div_res_zero_q & ~op_b_zero_q & ~op_b_neg_one_q) begin
           a_reg_en     = ab_comp;
           b_reg_en     = 1'b1;
           res_reg_en   = 1'b1;
         end
-        // can end the division now if the result is clearly 0
-        if(div_res_zero_q | op_b_zero_q) begin
+        // can end the division immediately if the result is known
+        if (div_res_zero_q | op_b_zero_q | op_b_neg_one_q) begin
           out_vld_o = 1'b1;
           state_d   = FINISH;
           if(out_rdy_i) begin
@@ -213,11 +212,11 @@ module serdiv import ariane_pkg::*; #(
 /////////////////////////////////////
 
   // get flags
-  assign rem_sel_d    = (load_en) ? opcode_i[1]               : rem_sel_q;
-  assign comp_inv_d   = (load_en) ? opcode_i[0] & op_b_sign   : comp_inv_q;
-  assign op_b_zero_d  = (load_en) ? op_b_zero                 : op_b_zero_q;
-  assign op_b_neg1_d  = (load_en) ? op_b_neg1                 : op_b_neg1_q;
-  assign res_inv_d    = (load_en) ? (~op_b_zero | opcode_i[1]) & opcode_i[0] & (op_a_sign ^ op_b_sign) : res_inv_q;
+  assign rem_sel_d       = (load_en) ? opcode_i[1]             : rem_sel_q;
+  assign comp_inv_d      = (load_en) ? opcode_i[0] & op_b_sign : comp_inv_q;
+  assign op_b_zero_d     = (load_en) ? op_b_zero               : op_b_zero_q;
+  assign op_b_neg_one_d  = (load_en) ? op_b_neg_one            : op_b_neg_one_q;
+  assign res_inv_d       = (load_en) ? (~op_b_zero | opcode_i[1]) & opcode_i[0] & (op_a_sign ^ op_b_sign) : res_inv_q;
 
   // transaction id
   assign id_d = (load_en) ? id_i : id_q;
@@ -240,7 +239,7 @@ module serdiv import ariane_pkg::*; #(
       comp_inv_q     <= 1'b0;
       res_inv_q      <= 1'b0;
       op_b_zero_q    <= 1'b0;
-      op_b_neg1_q    <= 1'b0;
+      op_b_neg_one_q <= 1'b0;
       div_res_zero_q <= 1'b0;
     end else begin
       state_q        <= state_d;
@@ -253,7 +252,7 @@ module serdiv import ariane_pkg::*; #(
       comp_inv_q     <= comp_inv_d;
       res_inv_q      <= res_inv_d;
       op_b_zero_q    <= op_b_zero_d;
-      op_b_neg1_q    <= op_b_neg1_d;
+      op_b_neg_one_q <= op_b_neg_one_d;
       div_res_zero_q <= div_res_zero_d;
     end
   end
