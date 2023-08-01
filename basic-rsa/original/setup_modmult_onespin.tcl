41ee75b59a00601939197f891c8000bc954b57ef
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2023-08-01 11:41:00 +0200

    Finished properties and latency analysis for the original design

---
+++ basic-rsa/original/setup_modmult_onespin.tcl
@@ -0,0 +1,17 @@
+# @lang=tcl @ts=8
+
+set script_path [file dirname [file normalize [info script]]]
+
+read_vhdl -golden  -pragma_ignore {}  -version 2008 {$script_path/modmult.vhd}
+
+set_elaborate_option -golden -vhdl_generic {mpwid=4}
+
+elaborate -golden
+
+compile -golden
+
+set_mode mv
+
+read_sva -version {sv2012} {$script_path//modmult.sva}
+
+check -verbose -all [get_checks]
